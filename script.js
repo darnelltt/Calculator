30a3b4039d2e0d2573de8f0b9800aaa75bca6556
Darnell GIttens <[darnellgittens16@gmail.com]>
2024-08-29 10:26:00 -0400

    Basic CSS Done

---
+++ script.js
@@ -0,0 +1,19 @@
+const firstNumber = ""
+const operator = ""
+const secondNumber = ""
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+    
+}
